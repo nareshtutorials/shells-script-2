a43f904b13b353c11a8d22434ee57e8920161b59
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-23 06:46:30 +0530

    Functions.sh file is added

---
+++ functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date + %F -%H-%M-%S)
+SCRIPT-NAME=$($0 | cut -d "." -f1)
+LoGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2 ...SUCCESS"
+    fi
+}
+
+if [$USERID -ne 0]
+
+then
+    echo "please run this script with root access"
+
+exit 1 # manually exit if error comes
+else
+    echo "you are superuser."
+fi
+
+dnf install mysql -y &>>$LOOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y
+VALIDATE $? "Installing Git"
+
