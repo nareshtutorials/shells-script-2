5bc94eb87c4ed075113d2c6a2c9b038c4499a40d
nareshtutorials <mnareshyadav885@gmail.com>
2024-04-23 20:04:51 +0530

    Functions.sh file is added

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT-NAME=$($0 | cut -d "." -f1)
+SCRIPT_NAME=$($0 | cut -d "." -f1)
 LoGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 
@@ -18,9 +18,9 @@ VALIDATE(){
 if [ $USERID -ne 0 ]
 
 then
-    echo "please run this script with root access"
+    echo "please run this script with root access."
 
-exit 1 # manually exit if error comes
+    exit 1 # manually exit if error comes.
 else
     echo "you are superuser."
 fi
